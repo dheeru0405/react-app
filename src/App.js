8c48afc7da96ee677c46871729e262a16e282fdc
Dheeraj Rane <dheerajrane29698@gmail.com>
2024-11-29 14:37:13 +0530

    Add react-app files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./style.css";
+import img from "./asserts/user1.jpg";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <div>
+        <img src={img} alt="user Image" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
